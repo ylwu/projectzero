d4aacd62c94fc5de7f92def9e4b8ce14b85832b2
Yonglin Wu <yonglinwu92@gmail.com>
2013-05-29 22:02:41 +0800

    Added 'Hello World' code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectzero::Application.config.secret_token = 'e03c6001f96103b46e4ad8ca8f56575a939f13295a5a933aa3529d205060d77646b4eaec90edcea1b7f43b2333d02e5e5993fee1802ae5ebc8390c72d89a535d'
